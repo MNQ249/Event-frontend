558f88d65283eb2f556f4975378820e80da2636d
Mesfer <mesfer.vip@gmail.com>
2019-12-18 17:03:24 +0300

    deploy

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const expressPort = 3000
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://boiling-lowlands-53514.herokuapp.com/',
   development: `http://localhost:${expressPort}`
 }
 
